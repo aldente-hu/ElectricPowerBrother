5f02a88470d83c6692dc84e869e6c0fe5b41a295
aldente-hu <aldente@hirosaki-u.ac.jp>
2017-06-12 13:02:35 +0900

    最終更新時刻の判定方法を変更。

--- ControlPanel/Environment.cs
+++ ControlPanel/Environment.cs
@@ -88,7 +88,6 @@ namespace HirosakiUniversity.Aldente.ElectricPowerBrother.ControlPanel
 		#region *コンストラクタ(Environment)
 		public Environment(string databaseFile, System.Xml.Linq.XElement loggers)
 		{
-			db = new ConsumptionRecorder(databaseFile);
 
 			foreach (var elem_logger in loggers.Elements())
 			{
@@ -113,6 +112,8 @@ namespace HirosakiUniversity.Aldente.ElectricPowerBrother.ControlPanel
 				logger.SetUp(elem_logger);
 				this.loggers.Add(logger);
 			}
+
+			db = new ConsumptionRecorder(databaseFile, this.loggers.SelectMany(l => l.Channels.SelectMany(ch => ch.Gains.Keys)).Distinct().ToArray());
 		}
 		#endregion
 
