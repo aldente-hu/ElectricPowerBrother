804dda03dbb7d48b938e1bc5b4f48cd31d5c4fbd
aldente-hu <aldente@hirosaki-u.ac.jp>
2017-06-13 10:58:29 +0900

    AtomGeneretorのバグを修正。

--- OutputData/MySQL/ConsumptionAtomGenerator.cs
+++ OutputData/MySQL/ConsumptionAtomGenerator.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
-using System.Data.SQLite;
+using MySql.Data.MySqlClient;
 
 namespace HirosakiUniversity.Aldente.ElectricPowerBrother.MySQL
 {
@@ -115,7 +115,7 @@ namespace HirosakiUniversity.Aldente.ElectricPowerBrother.MySQL
 				{
 					// ☆Commandの書き方は他にも用意されているのだろう(と信じたい)．
 					command.CommandText = "select ch, consumption from consumptions_10min where e_time = @1";
-					command.Parameters.Add(new SQLiteParameter("@1", TimeConverter.TimeToInt(time)));
+					command.Parameters.Add(new MySqlParameter("@1", TimeConverter.TimeToInt(time)));
 
 					using (var reader = command.ExecuteReader())
 					{
